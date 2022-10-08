ca0c30b5ea550713b3a2e8bf6df601947f123dbe
ZIN MIN PAING <zinmin75@gmail.com>
2022-10-08 14:57:17 +0900

    first commmit

---
+++ torch_.py
@@ -0,0 +1,128 @@
+import wandb
+wandb.login()
+
+import torch
+from torch.nn import CrossEntropyLoss, functional as F
+import torch.nn as nn
+from torch.optim import Adam
+from torchmetrics.functional import accuracy
+from pytorch_lightning import LightningModule
+from torchvision.datasets import CIFAR10
+from torchvision import transforms
+from torch.utils.data import DataLoader, random_split
+from pytorch_lightning.loggers import WandbLogger
+from pytorch_lightning import Trainer
+from pytorch_lightning.callbacks import Callback
+from pytorch_lightning.callbacks import ModelCheckpoint
+
+transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+
+dataset = CIFAR10(root="./CIFAR10", download=True, transform=transform)
+print(len(dataset))
+lengths = int(len(dataset)*0.8), int(len(dataset)*0.2)
+training_set, validation_set = random_split(dataset, lengths)
+training_loader = DataLoader(training_set, batch_size=64, shuffle=True)
+validation_loader = DataLoader(validation_set, batch_size=64)
+
+class CIFAR10_LitModule(LightningModule):
+
+    def __init__(self, n_classes=10, n_layer_1=128, n_layer_2=256, lr=1e-3):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+        # loss
+        self.loss = CrossEntropyLoss()
+
+        # optimizer parameters
+        self.lr = lr
+
+        # save hyper-parameters to self.hparams (auto-logged by W&B)
+        self.save_hyperparameters()
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+    def training_step(self, batch, batch_idx):
+        '''needs to return a loss from a single batch'''
+        _, loss, acc = self._get_preds_loss_accuracy(batch)
+
+        # Log loss and metric
+        self.log('train_loss', loss)
+        self.log('train_accuracy', acc)
+
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        '''used for logging metrics'''
+        preds, loss, acc = self._get_preds_loss_accuracy(batch)
+
+        # Log loss and metric
+        self.log('val_loss', loss)
+        self.log('val_accuracy', acc)
+        return preds
+
+    def test_step(self, batch, batch_idx):
+        '''used for logging metrics'''
+        _, loss, acc = self._get_preds_loss_accuracy(batch)
+
+        # Log loss and metric
+        self.log('test_loss', loss)
+        self.log('test_accuracy', acc)
+    
+    def configure_optimizers(self):
+        '''defines model optimizer'''
+        return Adam(self.parameters(), lr=self.lr)
+    
+    def _get_preds_loss_accuracy(self, batch):
+        '''convenience function since train/valid/test steps are similar'''
+        x, y = batch
+        logits = self(x)
+        preds = torch.argmax(logits, dim=1)
+        loss = self.loss(logits, y)
+        acc = accuracy(preds, y)
+        return preds, loss, acc
+
+model = CIFAR10_LitModule(n_layer_1=128, n_layer_2=128)
+checkpoint_callback = ModelCheckpoint(monitor='val_accuracy', mode='max')
+wandb_logger = WandbLogger(project='CIFAR10', # group runs in "MNIST" project
+                           log_model='all') # log all new checkpoints during training 
+class LogPredictionsCallback(Callback):
+    def on_validation_batch_end(
+        self, trainer, pl_module, outputs, batch, batch_idx, dataloader_idx):
+        """Called when the validation batch ends."""
+        if batch_idx == 0:
+            n = 20
+            x, y = batch
+            images = [img for img in x[:n]]
+            captions = [f'Ground Truth: {y_i} - Prediction: {y_pred}' for y_i, y_pred in zip(y[:n], outputs[:n])]
+            
+            # Option 1: log images with `WandbLogger.log_image`
+            wandb_logger.log_image(key='sample_images', images=images, caption=captions)
+
+            # Option 2: log predictions as a Table
+            columns = ['image', 'ground truth', 'prediction']
+            data = [[wandb.Image(x_i), y_i, y_pred] for x_i, y_i, y_pred in list(zip(x[:n], y[:n], outputs[:n]))]
+            wandb_logger.log_table(key='sample_table', columns=columns, data=data)
+
+log_predictions_callback = LogPredictionsCallback()
+trainer = Trainer(
+    logger=wandb_logger,                    # W&B integration
+    callbacks=[log_predictions_callback,    # logging of sample predictions
+               checkpoint_callback],        # our model checkpoint callback
+    max_epochs=20)            
+trainer.fit(model, training_loader, validation_loader)
+wandb.finish()
